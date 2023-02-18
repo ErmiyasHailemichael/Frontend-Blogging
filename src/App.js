43977f17b1b7f51b10f3cf63050495bd307635e2
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-02-18 01:47:36 -0800

    add three components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ArticleCard from './components/ArticleCard';
+import ArticleView from './components/ArticleView';
+import Editor from './components/Editor';
 
 function App() {
   return (
     <div className="App">
-     
+     <ArticleCard />
+     <ArticleView />
+     <Editor />
     </div>
   );
 }
---
+++ src/components/ArticleView.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ArticleView() {
+  return (
+    <div>ArticleView</div>
+  )
+}
