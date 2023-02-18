0be2e59c89495a28279e265088520661f899a893
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-02-18 02:28:33 -0800

    Editor add

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Editor() {
+const Editor = ({onsubmit}) => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onsubmit({title, content})
+  };
+
   return (
-    <div>Editor</div>
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
   )
 }
+
+export default Editor
