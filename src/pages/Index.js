3a51204166a74d9fb09caa30521d8f4a1675131e
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-03-13 00:11:41 -0700

    Tailwindcss is working

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import App from '../App.css';
 
-export default function Index({blogs, createBlogs}) {
+export default function Index({ blogs, createBlogs }) {
   const [form, setForm] = useState({
     title: '',
     body: '',
-    author: '',  
+    author: '',
   });
 
   const handleChange = (e) => {
@@ -16,7 +15,7 @@ export default function Index({blogs, createBlogs}) {
         [e.target.name]: e.target.value,
       };
     });
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,72 +23,78 @@ export default function Index({blogs, createBlogs}) {
     setForm({
       title: '',
       body: '',
-      author: ''
+      author: '',
     });
   };
 
   const reading = (str, num) => {
     return str?.length > num ? str.slice(0, num) + '...' : str;
-  }
+  };
 
   const wordCount = (str) => {
     return str?.split(' ').length;
-  }
+  };
 
   const timeToRead = (str) => {
     const wordsPerMinute = 200;
     const words = wordCount(str);
     return Math.ceil(words / wordsPerMinute);
-  }
+  };
 
-
-  const loaded = () => 
+  const loaded = () =>
     blogs.map((blog) => (
-      <div key={blog.id} className='card'>
-        <Link to={`/blog/${blog.id}`}>
-          <h1>{blog.title}</h1>
-        </Link>
-        <h3>{blog.author}</h3>
-        <p>{reading(blog.body, 50)}</p>
-        <span>{timeToRead(blog.body)} min read</span>
-        <Link to={`/blog/${blog.id}`} className="read-more-link">
-          Read More
-        </Link>
+      <div key={blog.id} className="max-w-xl mx-auto mt-10 bg-white rounded-xl shadow-md overflow-hidden">
+        <div className="p-6">
+          <Link to={`/blog/${blog.id}`}>
+            <h1 className="text-2xl font-bold text-gray-900 mb-2">{blog.title}</h1>
+          </Link>
+          <h3 className="text-gray-600 mb-2">{blog.author}</h3>
+          <p className="text-gray-800 mb-2">{reading(blog.body, 50)}</p>
+          <span className="text-gray-600">{timeToRead(blog.body)} min read</span>
+          <Link to={`/blog/${blog.id}`} className="block mt-4 text-blue-500 hover:text-blue-700 font-semibold">
+            Read More
+          </Link>
+        </div>
       </div>
     ));
 
   const loading = () => {
     return <h1>Loading...</h1>;
-  } 
+  };
 
   return (
-    <section className='container'>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          name='title'
-          value={form.title}
-          onChange={handleChange}
-          placeholder='Title'
-        />
-        <input
-          type='text'
-          name='body'
-          value={form.body}
-          onChange={handleChange}
-          placeholder='Body'
-        />
-        <input
-          type='text'
-          name='author'
-          value={form.author}
-          onChange={handleChange}
-          placeholder='Author'
-        />
-        <input type='submit' value='Publish' />
-      </form>
-      {blogs ? loaded() : loading()}
-    </section>
-      
+    <>
+      <section className="container mx-auto p-6">
+        <form onSubmit={handleSubmit} className="mb-8">
+          <input
+            type="text"
+            name="title"
+            value={form.title}
+            onChange={handleChange}
+            placeholder="Title"
+            className="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full"
+          />
+          <textarea
+            name="body"
+            value={form.body}
+            onChange={handleChange}
+            placeholder="Body"
+            className="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full h-32"
+          ></textarea>
+          <input
+            type="text"
+            name="author"
+            value={form.author}
+            onChange={handleChange}
+            placeholder="Author"
+            className="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full"
+          />
+          <input type="submit" value="Publish" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+        </form>
+      </section>
+      <section className="container mx-auto mt-8">
+        {blogs ? loaded() : loading()}
+      </section>
+    </>
   );
 }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,8 +1,8 @@
-import {useState} from 'react';
-import {useParams, useNavigate} from 'react-router-dom';
+import { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
-export default function Show({blogs, updateBlogs, deleteBlogs}) {
-  const {id} = useParams();
+export default function Show({ blogs, updateBlogs, deleteBlogs }) {
+  const { id } = useParams();
   const blog = blogs.find((blog) => blog.id === parseInt(id));
   const navigate = useNavigate();
 
@@ -13,48 +13,60 @@ export default function Show({blogs, updateBlogs, deleteBlogs}) {
       ...ediform,
       [e.target.name]: e.target.value,
     });
-    };
+  };
 
   const handleUpdate = (e) => {
     e.preventDefault();
     updateBlogs(ediform, id);
-    navigate('/');
-  }
+    navigate("/");
+  };
 
   const removeBlog = () => {
     deleteBlogs(id);
-    navigate('/');
+    navigate("/");
   };
 
   return (
-    <div>
-      <h1>{blog.title}</h1>
-      <h3>{blog.author}</h3>
-      <p>{blog.body}</p>
-      <button id = 'Delete' onClick={removeBlog}>Delete</button>
-      <form onSubmit={handleUpdate}>
+    <div className="bg-white p-4 rounded-lg shadow-md">
+      <h1 className="text-3xl font-bold mb-4">{blog.title}</h1>
+      <h3 className="text-lg font-medium mb-2">{blog.author}</h3>
+      <p className="text-gray-600 mb-4">{blog.body}</p>
+      <button
+        className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-lg mr-2"
+        onClick={removeBlog}
+      >
+        Delete
+      </button>
+      <form onSubmit={handleUpdate} className="flex flex-col">
         <input
-          type='text'
-          name='title'
+          className="mb-2 p-2 rounded-lg border border-gray-400"
+          type="text"
+          name="title"
           value={ediform.title}
           onChange={handleChange}
-          placeholder='Title'
+          placeholder="Title"
         />
         <input
-          type='text'
-          name='body'
+          className="mb-2 p-2 rounded-lg border border-gray-400"
+          type="text"
+          name="body"
           value={ediform.body}
           onChange={handleChange}
-          placeholder='Body'
+          placeholder="Body"
         />
         <input
-          type='text'
-          name='author'
+          className="mb-4 p-2 rounded-lg border border-gray-400"
+          type="text"
+          name="author"
           value={ediform.author}
           onChange={handleChange}
-          placeholder='Author'
+          placeholder="Author"
         />
-        <input type='submit' value='Update' />
+        <input
+          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg"
+          type="submit"
+          value="Update"
+        />
       </form>
     </div>
   );
